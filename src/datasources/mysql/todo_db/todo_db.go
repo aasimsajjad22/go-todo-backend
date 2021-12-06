b0701e7aea1f68b5fe259a193b7bfd2690053c8d
Aasim Sajjad <47242976+aasimsajjad22@users.noreply.github.com>
2021-12-06 18:27:16 +0500

    add env variable for db configuration

--- src/datasources/mysql/todo_db/todo_db.go
+++ src/datasources/mysql/todo_db/todo_db.go
@@ -24,8 +24,8 @@ var (
 )
 
 func init() {
-	//dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "aasim", "Ciklum@1234567$", "127.0.0.1:3306", "todo")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
+	//dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "", "", "", "")
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
